1196364d8fed5b8b0208a06803e06870cd14a0c6
Aaron Parecki <aaron@parecki.com>
2018-01-25 06:49:54 -0800

    stub test for testing travis setup

--- tests/FormEncodedInputTest.php
+++ tests/FormEncodedInputTest.php
@@ -1,4 +1,8 @@
 <?php
 class FormEncodedInputTest extends PHPUnit_Framework_TestCase {
 
+  public function testTest() {
+    $this->assertEquals(1, 1);
+  }
+
 }
--- tests/JSONInputTest.php
+++ tests/JSONInputTest.php
@@ -1,4 +1,8 @@
 <?php
 class JSONInputTest extends PHPUnit_Framework_TestCase {
 
+  public function testTest() {
+    $this->assertEquals(1, 1);
+  }
+
 }
